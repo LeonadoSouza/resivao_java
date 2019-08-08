6e249d0d24c822f8b79a1c03d559aea44bf017fb
LeonadoSouza <leonardo.souza1@universo.univates.br>
2019-08-08 19:55:36 -0300

    Update ClassePrincipal.java

--- src/univates/ClassePrincipal.java
+++ src/univates/ClassePrincipal.java
@@ -1,55 +1,71 @@
-package univates;
+package projeto;
 
 /*
-autor Leonardo
+   autor: Jonas Dhein
 */
 
+public class RevisaoJava {
 
-public class ClassePrincipal {
-
+    //método de inicialização da classe
     public static void main(String[] args) {
-        //instaciar uma clase
-        Funcoes objFuncoes = new Funcoes();
-        int resultado = objFuncoes.soma(2, 3);
-        System.out.println("Resultado da soma é = "+ resultado);
-        int result = objFuncoes.subtracao(4, 5);
-        System.out.println("Resultado da soma é = "+ result);
         
+        System.out.println("Olá Mundo");
+        int resultado = Funcoes.somarNumeros(1, 2);
+        System.out.println("Resultado = " + resultado);
         
-        //teste condicional(verdadeiro ou falso)
-        String var1 = "Juca";
-        String var2 = "juca";
-        //equalsIgnoreCase = compara texto ignorando o caseSensitive
-        if (!var1.equals(var2)) {
-            System.out.println("IF");
-        } else {
-            System.out.println("ELSE");
-        }
-        // laços de repetição
-        for (int i = 0; i < 10; i++) {
+        //criamos uma instância de Funcoes
+        Funcoes func = new Funcoes();
+        resultado = func.subtrairNumeros(4, 1);
+        System.out.println("Resultado = " + resultado);
+        
+        imprimirMensagem("TESTE 1");
+        imprimirMensagem("TESTE 2");
+        imprimirMensagem("TESTE 3");
+        
+        for(int i = 0; i <10; i++){
             System.out.println("valor de i = " + i);
         }
-        int i = 0;
-        while (i < 10) {
-            System.out.println("valor de i = " + i);
-            i += 1;
+        
+        int x = 15;
+        while( x < 18 ){
+            System.out.println("Sem permissão!!!");
+            x++;
         }
-        i = 0;
-        do {
-            System.out.println("valor de i = " + i);
-            i++;
-        } while (i < 10);
-        //tipos de variaveis
-        boolean n1 = false;
-        int n2 = 10;
-        float n3 = 5.4f;
-        System.out.println(n3);
-        double n4 = 5.3241524532452346234234;
-        System.out.println(n4);
-        String n5 = "Juca Bala";
-        System.out.println(n5.trim());//remive espaços
-        System.out.println(n5.trim().toUpperCase().length());
+        
+        int[] numeros = {1,2,3};
+        System.out.println("ENCHANCED FOR");
+        for(int var : numeros){
+            System.out.println(var);
+        }
+        
+        System.out.println("FOR COMUM");
+        for(int var = 0; var < numeros.length; var++){
+            System.out.println(numeros[var]);
+        }
+        
+        int[] numeros_array = {2,2,1};
+        int res_soma = Funcoes.somarNumeros(numeros_array);
+        System.out.println("RESULTADO DA SOMA = " + res_soma);
+        
+        int res_mul = Funcoes.multiplicaNumeros(numeros_array);
+        System.out.println("RESULTADO DA MULTIPLICAÇÃO = " + res_mul);
+        
+        double res_div = Funcoes.divideNumeros(numeros_array);
+        System.out.println("RESULTADO DA DIVISAO = " + res_div);
+        
+        int res_sub = Funcoes.subtraiNumeros(numeros_array);
+        System.out.println("RESULTADO DA SUBTRACAO = " + res_sub);
+        
+        System.out.println();//linha em branco
+        System.out.println();//linha em branco
+        System.out.println();//linha em branco
+        String sorteio = Funcoes.sortearNumeros(6, 60);
+        System.out.println("SORTEIO = " + sorteio);
+        
     }
-
+    
+    public static void imprimirMensagem(String par1){
+        System.out.println();
+    }
     
 }
